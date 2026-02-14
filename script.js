dafcda7d4f46096c6a9cafc88e9613a0377e2c12
VnSandWicth <123831880+VnSandWicth@users.noreply.github.com>
2026-02-14 21:02:46 +0700

    f

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ window.lastNotifiedTier = "";
 // 3. PRODUCT DATA & RENDERING UTILS
 // ==========================================
 var manualProducts = [
-    { name: "Aren", price: 15000, type: "signature", image: "img/gula aren.png", mg: 80, cost: 9000 },
+    { name: "Aren", price: 100, type: "signature", image: "img/gula aren.png", mg: 80, cost: 9000 },
     { name: "Salted Caramel ", price: 15000, type: "signature", image: "img/salted.png", mg: 85, cost: 9000 },
     { name: "Spanish Coffee", price: 15000, type: "signature", image: "img/spanish.png", mg: 85, cost: 9000 },
     { name: "Pistachio", price: 15000, type: "signature", image: "img/pistacio.png", mg: 85, cost: 9000 },
@@ -1509,4 +1509,5 @@ window.unbindGoPay = function () {
             location.reload();
         });
     }
+
 }
